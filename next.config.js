158497f86c40c7abff635a673800a237be9cf29b
ayoubmoudrika <33069191+ayoubmoudrika@users.noreply.github.com>
2025-09-19 16:46:08 -0400

    Fix GitHub Pages asset paths

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: process.env.GITHUB_PAGES ? '/Moments' : '',
+  assetPrefix: process.env.GITHUB_PAGES ? '/Moments/' : '',
   images: {
     unoptimized: true
   }
